c6e5b69a3efe876e5d4269f3f99df448b66c7b8f
ArtGenesio <arturg0222@gmail.com>
2023-09-03 15:37:33 +0200

    slider v.1_0

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let divisor = document.getElementsByClassName("divisor")
+let slider = document.getElementById("sliderRange")
+
+function divisorChanged() {
+    divisor.style.width = slider.value + "%"
+}*/
+
+let divisor = document.querySelector(".divisor");
+let slider = document.getElementById("sliderRange");
+
+        function divisorChanged() {
+            divisor.style.width = slider.value + "%";
+        }
